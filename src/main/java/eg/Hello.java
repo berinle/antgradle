36d32dd402503847cd60a22dff5d7d09590f204c
Bayo Erinle <berinle@gmail.com>
2010-11-27 16:15:11 -0500

    first commit

---
+++ src/main/java/eg/Hello.java
@@ -0,0 +1,11 @@
+package eg;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bayo
+ * Date: Nov 27, 2010
+ * Time: 3:49:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+}
